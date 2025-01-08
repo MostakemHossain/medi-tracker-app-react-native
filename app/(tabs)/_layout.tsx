a32fa63561a52df656554b5630cfaab20f0559ed
MostakemHossain <mostakememon123@gmail.com>
2025-01-08 10:34:35 +0600

    handle Tabls menu

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,43 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="AddNew"
+        options={{
+          tabBarLabel: "Add New",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="plus-square" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
