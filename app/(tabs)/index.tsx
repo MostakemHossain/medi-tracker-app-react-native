232e27a35f82d5e7e9b65208935edd3543c15efb
MostakemHossain <mostakememon123@gmail.com>
2025-01-08 11:49:46 +0600

    login page redrict page added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
+import { Redirect } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function index() {
+export default function HomeScreen() {
   return (
     <View>
       <Text>Home Screen</Text>
+      <Redirect href={"/login"} />
     </View>
   );
 }
---
+++ app/login/index.tsx
@@ -0,0 +1,74 @@
+import Colors from "@/ constant/Colors";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 40,
+        }}
+      >
+        <Image
+          source={require("../../assets/images/login.png")}
+          style={styles.image}
+        />
+      </View>
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "100%",
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 30,
+            textAlign: "center",
+          }}
+        >
+          Stay on Track ,Stay Healthy
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 17,
+            textAlign: "center",
+            marginTop: 20,
+          }}
+        >
+          Track your medicine,take control of your health. Stay consistent, Stay
+          confident.
+        </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text
+            style={{ textAlign: "center", fontSize: 16, color: Colors.PRIMARY }}
+          >
+            Continue
+          </Text>
+        </TouchableOpacity>
+        <Text style={{ color: "white", marginTop: 8, textAlign: "center" }}>
+          Note: By Clicking Continue button,you will agree to our terms &
+          conditions
+        </Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  image: {
+    width: 210,
+    height: 450,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: "white",
+    borderRadius: 99,
+    marginTop: 25,
+  },
+});
